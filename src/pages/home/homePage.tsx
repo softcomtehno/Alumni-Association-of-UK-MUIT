2c998972007f833005bb293a980a559ffccbab22
sartovAkhmed <akhmedturan@gmail.com>
2025-04-28 20:14:51 +0600

    fix: app/index.css animate.css resolve

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,9 +1,15 @@
-import { Button } from "~app/components/ui/button";
+import { useState } from "react";
+import { Calendar } from "~app/components/ui/calendar";
 
 export const HomePage: React.FC = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
   return (
-    <div>
-      <Button>Click me!</Button>
-    </div>
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border"
+    />
   );
 };
