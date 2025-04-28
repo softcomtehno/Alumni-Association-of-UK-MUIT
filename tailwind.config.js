cfb72e572225a9deac3d0c5a7024116e2ae65a8d
sartovAkhmed <akhmedturan@gmail.com>
2025-04-28 19:56:11 +0600

    feat: eslint and prettier plagins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   important: "#root",
   theme: {
     fontFamily: {
       serif: ["Geologica", "serif"],
+      sans: ["Geologica", "serif"],
     },
     colors: {
       primary: "#0D9488",
@@ -22,24 +23,114 @@ export default {
       black: "black",
       violet: "#8f95cd",
       milk: "#fe98c8",
+      tandaBtnBg: "#005B50",
+      tandaColor: "#1F1F1F",
+      tandaTestBg: "rgb(245, 245, 245)",
+      TandaBg: "rgb(255, 255, 255)",
     },
     extend: {
-      // spacing: {
-      //   'container-default': '65px',
-      //   'container-xl': '50px',
-      //   'container-lg': '40px',
-      //   'container-md': '30px',
-      //   'container-sm': '20px',
-      //   'container-xs': '15px',
-      // },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
     },
     screens: {
-      "r-xl": { max: "1440px" },
-      "r-lg": { max: "1024px" },
-      "r-md": { max: "768px" },
-      "r-sm": { max: "480px" },
-      "r-xs": { max: "360px" },
+      "r-xl": {
+        max: "1440px",
+      },
+      "r-lg": {
+        max: "1024px",
+      },
+      "r-md": {
+        max: "768px",
+      },
+      "r-sm": {
+        max: "480px",
+      },
+      "r-xs": {
+        max: "360px",
+      },
+      "m-xl": {
+        min: "1440px",
+      },
+      "m-lg": {
+        min: "1024px",
+      },
+      "m-md": {
+        min: "768px",
+      },
+      "m-sm": {
+        min: "480px",
+      },
+      "m-xs": {
+        min: "360px",
+      },
+      "t-xl": {
+        max: "1170px",
+      },
+      "t-lg": {
+        max: "1075px",
+      },
+      "t-m": {
+        max: "900px",
+      },
+      "t-md": {
+        max: "640px",
+      },
+      "t-x": {
+        max: "530px",
+      },
+      "t-sm": {
+        max: "430px",
+      },
+      "t-xs": {
+        max: "380px",
+      },
     },
     plugins: [],
   },
+  plugins: [require("tailwindcss-animate")],
 };
